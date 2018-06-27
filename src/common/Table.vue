0968e8a545e71710ece02b9f67176738db92a237
gyxing <172176234@qq.com>
2018-06-28 00:16:39 +0800

    table

---
+++ src/common/Table.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="b-table">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Table",
+    props: {
+      fields: [
+        { label: '', field: 'index' },
+        { label: '', field: 'title' },
+      ]
+    },
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
